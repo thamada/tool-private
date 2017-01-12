855b7deff2332e95f65a74be9d8924c0c376d1de
Tsuyoshi Hamada <hamada@progrape.jp>
2017-01-12 23:50:08 +0900

    Wise men learn more from fools than fools from the wise. -- Cato the Elder

---
+++ comit.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python2.7
+# -*- coding:utf-8 -*-
+#
+# Copyright (c) 2017 by Tsuyoshi Hamada. All rights reserved.
+#
+
+import os
+import logging as LG
+import random
+import commands
+import shelve
+
+def get_logger():
+    # create logger
+    logger = LG.getLogger(os.path.basename(__file__))
+    logger.setLevel(LG.DEBUG)
+
+    # create console handler and set level to debug
+    ch = LG.StreamHandler()
+    ch.setLevel(LG.DEBUG)
+
+    # create formatter
+    formatter = LG.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # add formatter to ch
+    ch.setFormatter(formatter)
+
+    # add ch to logger
+    logger.addHandler(ch)
+
+    # 'application' code
+    ## logger.debug('debug message')
+    ## logger.info('info message')
+    ## logger.warn('warn message')
+    ## logger.error('error message')
+    ## logger.critical('critical message')
+    return logger
+
+def get_quotes():
+    result = [ ]
+    result.append(u"1. 生きているということ")
+    result.append(u"2. いま生きているということ")
+    result.append(u"3. それはのどがかわくということ")
+    result.append(u"4. 木漏れ日がまぶしいということ")
+    result.append(u"5. ふっと或るメロディを思い出すということ")
+    result.append(u"6. くしゃみをすること")
+    result.append(u"7. あなたと手をつなぐこと")
+    return result
+
+import pickle
+
+def test_shelve(logger):
+    fname = './shelve.dat'
+    keyname = 'count'
+    pickle_protocol = pickle.HIGHEST_PROTOCOL
+    dic = shelve.open(fname, protocol=pickle_protocol)
+    keys = dic.keys()
+    if keyname in keys:
+        logger.debug(keys)
+
+    if keyname not in keys: 
+        dic[keyname] = 0
+        dic.close()
+        dic = shelve.open(fname, protocol=pickle_protocol)
+
+    count = dic[keyname]
+    count += 1
+    dic[keyname] = count
+    dic.close()
+    return count
+
+if __name__ == "__main__":
+    logger = get_logger()
+    qs = get_quotes()
+    qs.sort()
+    msg = random.choice(qs)
+    cmd = 'git commit -m "' + msg + '"; git push origin master;'
+#    logger.info('### %s', msg)
+    print cmd.encode('euc-jp')
+    count = test_shelve(logger)
+    print "count=", count
